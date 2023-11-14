bca9d386d201db8457d62ca5ccba08668e0c8aa8
Timothyw0 <timothyw0@gmail.com>
2023-11-14 18:55:40 -0500

    Added Navbar component

---
+++ tim-react-website/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,111 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  HStack,
+  Heading,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Stack,
+  Text,
+  useColorMode,
+  useColorModeValue,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon, MoonIcon } from "@chakra-ui/icons";
+
+interface NavLinkProps {
+  action?: string;
+  children: React.ReactNode;
+}
+
+const Links = ["Home", "About Me", "Experience", "Portfolio"];
+
+const NavLink = (props: NavLinkProps) => {
+  const { action, children } = props;
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <Box
+      as="a"
+      px={2}
+      py={1}
+      rounded={"md"}
+      _hover={{
+        textDecoration: "none",
+        textColor: colorMode === "light" ? "black" : "white",
+        bg: useColorModeValue("gray.200", "gray.700"),
+      }}
+      href={"#"}
+      onClick={() => {
+        action === "switchMode" && toggleColorMode();
+      }}
+    >
+      {children}
+    </Box>
+  );
+};
+
+const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems="center" justifyContent="space-between">
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Box>
+              <Heading as="h1" size="lg">
+                Timothy Wang
+              </Heading>
+            </Box>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+              <NavLink action="switchMode">
+                <MoonIcon />
+              </NavLink>
+            </HStack>
+          </HStack>
+          <Flex alignItems="center">
+            <Avatar
+              size="md"
+              src="https://media.licdn.com/dms/image/C5603AQGFoEYhXtkaiw/profile-displayphoto-shrink_800_800/0/1592782396048?e=1705536000&v=beta&t=QBf5w8Oct5IZiiq_4rc983Y5CSkcLf4AFsejtuCK-ew"
+            />
+          </Flex>
+        </Flex>
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+              <NavLink action="switchMode">
+                <MoonIcon />
+              </NavLink>
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
+};
+
+export default Navbar;
--- tim-react-website/src/main.tsx
+++ tim-react-website/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ChakraProvider, Container } from "@chakra-ui/react";
+import "./index.css";
+import theme from "./theme.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ChakraProvider theme={theme}>
+    <Container minHeight="100vh" minWidth="100vw" p={0}>
+      <App />
+    </Container>
+  </ChakraProvider>
+);
